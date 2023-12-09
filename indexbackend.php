914f4411a2777ce476908a055041e3166baa830c
Vicky <116822525+ItsVicky25@users.noreply.github.com>
2023-12-09 20:21:49 +0530

    Add files via upload

---
+++ indexbackend.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Downlod File</title>
+</head>
+<body>
+    <center>
+
+    <h2 style="color:#d7385e;">Download</h2>
+    <p><b>click below to download PDF</b></p>
+    <a href="download.php?file=pro" target="_rathaur">Downlod PDF Now</a>
+
+
+    </center>
+</body>
+</html>
